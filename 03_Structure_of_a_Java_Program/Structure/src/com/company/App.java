84c5f8b530ba79907e062be05eb84e7aa447599f
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-15 18:56:47 +0530

    Add Structure of a Java Program

---
+++ 03_Structure_of_a_Java_Program/Structure/src/com/company/App.java
@@ -0,0 +1,64 @@
+/*
+    ? Package
+    A package in Java is used to group related classes.
+    Think of it as a folder in a file directory.
+*/
+package com.company;
+
+/*
+    ? Classs
+    A class is a template for objects.
+    It is a collection of variables and methods.
+    A class is a blueprint from which individual objects are created.
+*/
+
+/*
+    ? Naming Conventions:
+    * Class Names - PascalCase
+    * Method/Function Names - camelCase
+*/
+
+/*
+    ? Anatomy of a Java Program
+    * Documentation Section
+    * Package Statement
+    * Import Statements
+    * Interface Statements
+    * Class Definitions
+    * Main Method Class
+    * {
+    * Main Method Definition
+    * }
+ */
+
+/*
+ ? Data Types
+    * Primitive Data Types
+        * Integer Types
+            * byte
+            * short
+            * int
+            * long
+        * Floating Point Types
+            * float
+            * double
+        * Character Type
+            * char
+        * Boolean Type
+            * boolean
+    * Non-Primitive Data Types
+        * String
+        * Arrays
+        * Classes
+        * Interfaces
+        * Enumerations
+        * Annotations
+        * Lambda Expressions
+ */
+
+public class App {
+    public static void main(String[] args) // main() method is the entry point of the program
+    {
+        System.out.println("Hello World!");
+    }
+}
