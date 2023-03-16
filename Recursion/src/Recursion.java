cb76aaa7f39e21f1aebf53f1bf2feb00c9e980b0
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-16 19:24:49 +0530

    Add Recursion

---
+++ Recursion/src/Recursion.java
@@ -0,0 +1,16 @@
+public class Recursion {
+
+    static int fact(int n) {
+        if (n <= 1)
+            return 1;
+
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        System.out.printf("%d! = %d", x, fact(x));
+
+    }
+}
