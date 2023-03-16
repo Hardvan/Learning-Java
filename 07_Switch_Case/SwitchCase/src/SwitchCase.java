d8863f9155698faf47f9b303f883611c5be77a66
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-16 18:22:05 +0530

    Add Switch Case

---
+++ 07_Switch_Case/SwitchCase/src/SwitchCase.java
@@ -0,0 +1,20 @@
+
+public class SwitchCase {
+    public static void main(String[] args) {
+
+        String str = "Hardvan";
+
+        // ? We can use switch case with Strings in Java
+        switch (str) {
+            case "Hardvan":
+                System.out.println("Hello Hardvan");
+                break;
+            case "Hardik":
+                System.out.println("Hello Hardik");
+                break;
+            default:
+                System.out.println("Hello World");
+        }
+
+    }
+}
