46dce8865ca301eb17336a19627b5c011e17ddae
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-03-16 19:24:45 +0530

    Add VarArgs

---
+++ 11_VarArgs/src/VarArgs.java
@@ -0,0 +1,25 @@
+public class VarArgs {
+
+    // ? VarArgs - Variable Arguments
+    // 0 or more arguments
+    // VarArgs must be the last argument
+    static int sum(int... arr) {
+        int result = 0;
+        for (int i : arr)
+            result += i;
+
+        return result;
+    }
+
+    // Atleast one argument is required
+    static void fun(int x, int... arr) {
+        return;
+    }
+
+    public static void main(String[] args) {
+
+        int result = sum(1, 2, 3, 4, 5);
+        System.out.println("Sum: " + result);
+
+    }
+}
